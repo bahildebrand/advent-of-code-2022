e7ad9728fb979a64ec79804a947509e2edae4328
Blake Hildebrand <baugusthildebrand@gmail.com>
2022-12-04 21:52:53 -0500

    Day 4

---
+++ src/day_4.rs
@@ -0,0 +1,101 @@
+use std::cmp::max;
+use std::io::BufRead;
+use std::{cmp::min, fs::File};
+
+use anyhow::{bail, Error, Result};
+
+struct SectionRange {
+    start: u32,
+    end: u32,
+}
+
+impl SectionRange {
+    fn contains(&self, other: &SectionRange) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+}
+
+impl TryFrom<&str> for SectionRange {
+    type Error = Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let tokens = value.split("-").collect::<Vec<_>>();
+        if tokens.len() != 2 {
+            bail!("Invalid SectionRange string: {}", value);
+        }
+
+        let start = tokens[0].parse::<u32>()?;
+        let end = tokens[1].parse::<u32>()?;
+
+        Ok(Self { start, end })
+    }
+}
+
+struct ElfPair {
+    elf1: SectionRange,
+    elf2: SectionRange,
+}
+
+impl TryFrom<String> for ElfPair {
+    type Error = Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let tokens = value.split(",").collect::<Vec<_>>();
+        if tokens.len() != 2 {
+            bail!("Invalid SectionRange string: {}", value);
+        }
+
+        let elf1 = SectionRange::try_from(tokens[0])?;
+        let elf2 = SectionRange::try_from(tokens[1])?;
+
+        Ok(Self { elf1, elf2 })
+    }
+}
+
+pub fn day_4() -> Result<()> {
+    day_4_1()?;
+    day_4_2()
+}
+
+fn day_4_1() -> Result<()> {
+    fn full_overlap(pair: &ElfPair) -> bool {
+        pair.elf1.contains(&pair.elf2) || pair.elf2.contains(&pair.elf1)
+    }
+
+    let sum = day_4_inner(full_overlap)?;
+
+    println!("Day 4-1: {}", sum);
+
+    Ok(())
+}
+
+fn day_4_2() -> Result<()> {
+    fn partial_overlap(pair: &ElfPair) -> bool {
+        pair.elf1.start <= pair.elf2.end && pair.elf2.start <= pair.elf1.end
+    }
+
+    let sum = day_4_inner(partial_overlap)?;
+
+    println!("Day 4-2: {}", sum);
+
+    Ok(())
+}
+
+type CompFunction = fn(&ElfPair) -> bool;
+
+fn day_4_inner(comp_fn: CompFunction) -> Result<u32> {
+    let file = File::open("input/day_4.txt")?;
+
+    let mut sum = 0u32;
+    let lines = std::io::BufReader::new(file).lines();
+    for line in lines {
+        let line = line?;
+
+        let elf_pair = ElfPair::try_from(line)?;
+        if comp_fn(&elf_pair) {
+            sum += 1;
+        }
+    }
+
+    Ok(sum)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 use anyhow::Result;
 
 use day_1::day_1;
 use day_2::day_2;
 use day_3::day_3;
+use day_4::day_4;
 
 fn main() -> Result<()> {
     day_1()?;
     day_2()?;
     day_3()?;
+    day_4()?;
 
     Ok(())
 }
